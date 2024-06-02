ce43962f920e6645791bf9c51c69cbcca5d20d04
Piotr Zaniewski <piotrzan@gmail.com>
2024-06-02 18:52:59 +0200

    slides
    Signed-off-by: Piotr Zaniewski <piotrzan@gmail.com>

---
+++ slides/demo.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# The set -e option instructs bash to immediately exit if any command has a non-zero exit status
+# The set -u referencing a previously undefined  variable - with the exceptions of $* and $@ - is an error
+# The set -o pipefaile if any command in a pipeline fails, that return code will be used as the return code of the whole pipeline
+# https://bit.ly/37nFgin
+set -euo pipefail
+
+# Add source and line number wher running in debug mode: bash -xv intro.sh
+export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+
+if command -v figlet &>/dev/null && command -v boxes &>/dev/null; then
+	echo "DEMO TIME" | figlet -f pagga | boxes -d peek
+else
+	echo "DEMO TIME"
+fi
